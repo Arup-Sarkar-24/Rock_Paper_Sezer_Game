075110018305daf5daeba51789649f05f899d923
arup-sarkar-24 <1arupsarkar112@gmail.com>
2025-03-06 19:52:47 +0530

    4th commit

--- src/com/mycompany/game/Main.java
+++ src/com/mycompany/game/Main.java
@@ -6,24 +6,27 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Lets play Rock Paper Sezer game : ");
-        System.out.println("Where input 1 = Rock, 2 = Paper, 3 = Sezer");
-        String[] choice={"sezer","Paper","Rock"};
-        System.out.print("Enter the number how many time you want to play : ");
+        Random rand = new Random();
+
+        System.out.println("=================== Lets play Rock Paper Sezer game ============================\n");
+        System.out.println("Where input 1 = Rock, 2 = Paper, 3 = Sezer\n");
+        String[] choice={"Rock","Paper","sezer"};
+        System.out.print("Enter the number how many round you want to play : ");
         short number = sc.nextShort();
+        System.out.println();
         short playTime = 0;
         short correctInp= 0;
         short drowInp =0;
         while(playTime<number){
             System.out.print("Choose one of them ---> Rock \tPaper \tSezer : ");
             byte player_inp = sc.nextByte();
-            String player_str = choice[player_inp%3];
-            System.out.println("You select "+ player_str);
-            System.out.print(", Lets see what PC choose...");
+
+            String player_str = choice[player_inp%3>0 ? player_inp%3-1: player_inp%3+2];
+            System.out.println("You select "+ player_str+" ;");
+            System.out.print("Lets see what PC choose...");
 
             //Random Number Generate
 
-            Random rand = new Random();
             byte rand_int = (byte) (rand.nextInt(3));
             String pc_str=choice[rand_int];
             System.out.println("PC select : "+pc_str);
@@ -54,13 +57,14 @@ public class Main {
                     correctInp++;
                 }
             }
-            System.out.println("Score : "+ correctInp);
+            System.out.printf("Score : %.2f \n",((float)correctInp+((float)drowInp/2)));
             playTime ++;
-            System.out.println("Match no : "+playTime);
+            System.out.println("Round no of the match : "+playTime);
+            System.out.println("\n");
         }
-        System.out.printf("Total wins %s total match %s\n",playTime,number);
+        System.out.printf("Total round of the match %s \nTotal wins in match %s \nTotal draw in the match %s\n",number,correctInp,drowInp);
         float winning_rate=(((float) correctInp+((float) drowInp/2))/(float) number)*100;
-        System.out.printf("Your winning rate : %.2f",winning_rate);
-
+        System.out.printf("Your winning rate : %.2f\n",winning_rate);
+        System.out.println(winning_rate<30? "You are too week in guessing.........":"You are not a noob !!!!!!!!");
     }
 }
